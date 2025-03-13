94134cf3d25f5750b84d2816a61a7efa393018d8
Arushi9741 <mamathadevi394@gamil.com>
2025-03-13 18:56:15 +0530

    subtraction

---
+++ subtract.py
@@ -0,0 +1,8 @@
+def subtract(a,b):
+    return(a-b)
+
+if __name__=='__main__':
+    a = int(input("Enter a : "))
+    b = int(input("Enter b : "))
+    print(f"the reminder is : {subtract(a,b)}")
+    
